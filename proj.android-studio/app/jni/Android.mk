22c5dc604737c39dbaa6040060296ce077ce401e
yalab <rudeboyjet@gmail.com>
2016-10-27 16:53:27 +0900

    It can compile on Android.

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -10,11 +10,29 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+CPP_FILES := $(shell find $(LOCAL_PATH)/../../../Classes -name *.cpp | grep -v "\#")
+CPP_FILES += $(LOCAL_PATH)/../../../Vendor/lib/Touchable.cpp
+CPP_FILES += $(LOCAL_PATH)/../../../Vendor/lib/ImageManager.cpp
+CPP_FILES += $(LOCAL_PATH)/../../../Vendor/lib/BGMPlayer.cpp
+CPP_FILES += $(shell find $(LOCAL_PATH)/../../../Vendor/CCSocialShare/CCSocialShare/android -name *.cpp)
+CPP_FILES += $(shell find $(LOCAL_PATH)/../../../Vendor/CCFirebase/Classes -name *.cpp)
+CPP_FILES += $(shell find $(LOCAL_PATH)/../../../Vendor/CCFirebase/proj.android -name *.cpp)
+CPP_FILES += $(shell find $(LOCAL_PATH)/../../../Vendor/CCLocalNotification/Classes -name *.cpp)
+CPP_FILES += $(shell find $(LOCAL_PATH)/../../../Vendor/CCLocalNotification/proj.android -name *.cpp)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+
+LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(shell find $(LOCAL_PATH)/../../../Classes -type d)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../cocos2d/cocos/ui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../Vendor/lib
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../Vendor/PluginXProtocols/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../cocos2d/external/curl/include/android
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../Vendor/CCSocialShare/CCSocialShare
+LOCAL_C_INCLUDES += /opt/boxen/homebrew/Cellar/openssl/1.0.2e_1/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../Vendor/CCFirebase/Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../Vendor/CCLocalNotification/Classes
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
--- proj.android-studio/app/jni/Application.mk
+++ proj.android-studio/app/jni/Application.mk
@@ -1,8 +1,10 @@
-APP_STL := gnustl_static
+APP_STL := c++_static
+NDK_TOOLCHAIN_VERSION=clang
 
-APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++14 -fsigned-char -DCC_USE_3D_PHYSICS=0
 APP_LDFLAGS := -latomic
 
+APP_PLATFORM := android-19
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
